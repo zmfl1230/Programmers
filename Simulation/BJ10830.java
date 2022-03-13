a42892724b1513464ec7e93d7e0cbb1590524ca7
zmfl1230 <zmfl123097@gmail.com>
2022-03-13 23:02:16 +0900

    remove unnecessary import

--- Simulation/BJ10830.java
+++ Simulation/BJ10830.java
@@ -3,7 +3,6 @@ package Simulation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BJ10830 {
